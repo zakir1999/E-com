a9edfc0f72d513b0c631752bf960ba8e86ab1c2a
zakir1999 <m.zakirhossen99@gmail.com>
2025-04-09 18:03:32 +0600

    1:31

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,6 +1,28 @@
+
+'use client';
+
+import { Button } from "@/components/ui/button";
+import { useCartStore } from "@/store/cart-store";
+import { ShoppingCartIcon, Bars3Icon,XMarkIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function Navbar(){
+  const [mobileOpen,setMobileOpen]=useState<boolean>(false);
+  const {items}=useCartStore();
+  const cartCount=items.reduce((acc,item)=>acc+item.quantity,0);
+  useEffect(()=>{
+    const handleResize =()=>{
+      if(window.innerWidth > 768){
+        setMobileOpen(false);
+      }
+
+    };
+    window.addEventListener("resize",handleResize);
+    return ()=>{
+      window.removeEventListener("resize",handleResize);
+    }
+  },[])
     return (
      <nav className="sticky top-0 z-50 bg-white shadow">
       <div className="container mx-auto flex items-center justify-between px-4 py-4">
@@ -16,7 +38,18 @@ export default function Navbar(){
             Checkout
           </Link>
         </div>
-        <div className="flex items-center space-x-4"></div>
+        <div className="flex items-center space-x-4">
+
+          <Link href="/checkout">
+          
+          <ShoppingCartIcon />
+          {
+            cartCount > 0 && <span>{cartCount}</span>
+          }
+            </Link>
+            <Button variant="ghost" onClick={()=>setMobileOpen((prev)=>!prev)}> {mobileOpen ? (<XMarkIcon/>):<Bars3Icon/>}
+</Button>
+        </div>
         </div>
         </nav>
     );
